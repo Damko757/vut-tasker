267cfd217c4fc794f276ee51b6ce0c7e291da304
Damián Jambor <jambor.damian@gmail.com>
2024-09-26 00:08:42 +0200

    Cool user animation

--- web/src/components/Main/UserComponent.vue
+++ web/src/components/Main/UserComponent.vue
@@ -18,21 +18,58 @@ const props = defineProps({
             class="line me-1 rounded-3"
             :style="{ background: user.color }"
         ></div>
-        <div>{{ user.nick }}</div>
+        <div class="mx-3 w-fit-content position-relative nick px-1">
+            <div class="text-block-wrapper">
+                <div class="text-block rounded-2 px-1">{{ user.nick }}</div>
+            </div>
+            <div>{{ user.nick }}</div>
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
 @import "/src/SCSS/main.scss";
-.line {
-    width: 0.3em;
-    min-height: 100%;
-}
 .user {
+    .nick {
+        transition: inherit;
+    }
+
     cursor: pointer;
     transition: 250ms;
     color: $white !important;
+    position: relative;
+
     &:hover {
-        color: var(--color) !important;
+        .text-block-wrapper {
+            width: 100%;
+        }
+        .line {
+            left: 100%;
+            transform: translateX(-100%);
+        }
+    }
+
+    .line {
+        transition: inherit;
+        position: absolute;
+        left: 0;
+        width: 0.3em;
+        min-height: 100%;
+        z-index: 3;
+    }
+
+    .text-block-wrapper {
+        transition: inherit;
+        width: 0%;
+        height: 100%;
+        position: absolute;
+        overflow: hidden;
+
+        .text-block {
+            width: fit-content;
+            position: relative;
+            background-color: $white;
+            color: var(--color);
+        }
     }
 }
 </style>
