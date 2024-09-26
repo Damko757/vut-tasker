68654e9f2a3b9a87e00d5ec684684cde6d44b5d6
Damián Jambor <jambor.damian@gmail.com>
2024-09-26 10:37:05 +0200

    Added required values to users config

--- shared/config/users.ts
+++ shared/config/users.ts
@@ -5,15 +5,21 @@ export const USERS: User[] = [
         nick: "User1",
         fullname: "Userus",
         color: "#008000",
+        show_my_progress: true,
+        show_me_progress: true,
     },
     {
         nick: "User2",
         fullname: "Serus",
         color: "#250018",
+        show_my_progress: true,
+        show_me_progress: true,
     },
     {
         nick: "User3",
         fullname: "Sures",
         color: "#123456",
+        show_my_progress: true,
+        show_me_progress: true,
     },
 ];
