a35c789be12406a3e3eabbc100b71cae5cb53c49
Damián Jambor <jambor.damian@gmail.com>
2024-09-26 22:26:35 +0200

    Route between Home and Subject Screen

--- web/src/screens/MainScreen.vue
+++ web/src/screens/MainScreen.vue
@@ -1,34 +1,31 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import axios, { HttpStatusCode } from "axios";
-import type { Task } from "../../../shared/Entities/Task";
 import HomeScreen from "./HomeScreen.vue";
+import NavComponent from "../components/NavComponent.vue";
+import SubjectScreen from "./SubjectScreen.vue";
 
 const dots = ref("");
 
 const loadState = ref(0);
-const tasks = ref<Task[]>([]);
+
+const homeScreen = ref<InstanceType<typeof HomeScreen>>();
+const subjectScreen = ref<InstanceType<typeof SubjectScreen>>();
+
+const url = new URL(window.location.href);
+const subject = url.pathname.split("/").at(1)?.toLocaleUpperCase() ?? null;
 
 function load() {
-    loadState.value = 0;
     dots.value = "";
-    const dotInterval = setInterval(() => {
+    const dotsInterval = setInterval(() => {
         dots.value += ".";
-        if (loadState.value != 0) clearInterval(dotInterval);
+        if (loadState.value != 0) clearInterval(dotsInterval);
     }, 500);
-    axios
-        .get<Task[]>("http://localhost:3000/tasks")
-        .then((response) => {
-            if (response.status == HttpStatusCode.Ok) {
-                loadState.value = 1;
-                tasks.value = response.data;
-            } else {
-                loadState.value = -1;
-            }
-        })
-        .catch(() => {
-            loadState.value = -1;
-        });
+
+    if (subject) {
+        subjectScreen.value?.load();
+    } else {
+        homeScreen.value?.load();
+    }
 }
 
 onMounted(async () => {
@@ -36,27 +33,56 @@ onMounted(async () => {
 });
 </script>
 <template>
-    <div v-if="loadState == 0" class="fw-bold text-white p-3 fs-2">
-        Loading{{ dots }}
-    </div>
-    <div
-        v-else-if="loadState == -1"
-        class="fw-bold p-3 fs-1 text-danger text-center"
-    >
-        Unable to connect! :C
-        <button
-            type="button"
-            class="btn btn-success d-block mx-auto fw-bold"
-            @click="load"
+    <NavComponent />
+    <main>
+        <div
+            v-if="loadState == 0"
+            class="fw-bold text-white p-3 fs-2 text-wrap"
+            style="word-break: break-all"
         >
-            Refresh
-        </button>
-    </div>
-    <template v-else-if="loadState == 1">
-        <NavComponent />
-        <main>
-            <HomeScreen @load-state-change="(ns) => (loadState = ns)" />
-        </main>
-    </template>
+            Loading{{ dots }}
+        </div>
+        <div
+            v-else-if="loadState == -1"
+            class="fw-bold p-3 fs-1 text-danger text-center"
+        >
+            Unable to connect! :C
+            <button
+                type="button"
+                class="btn btn-success d-block mx-auto fw-bold"
+                @click="load"
+            >
+                Refresh
+            </button>
+        </div>
+        <div v-show="loadState == 1">
+            <SubjectScreen
+                v-if="subject"
+                ref="subjectScreen"
+                :subject-name="subject"
+                @load-state-change="
+                    (ns) => {
+                        console.info(ns);
+                        loadState = ns;
+                    }
+                "
+            />
+            <HomeScreen
+                v-else
+                ref="homeScreen"
+                @load-state-change="
+                    (ns) => {
+                        console.info(ns);
+                        loadState = ns;
+                    }
+                "
+            />
+        </div>
+    </main>
 </template>
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+@import "/src/SCSS/main.scss";
+main {
+    padding-left: 5rem;
+}
+</style>
