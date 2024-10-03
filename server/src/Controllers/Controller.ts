d6a15a14ad0cc62a24f19594dd3142b780c655e1
Damián Jambor <jambor.damian@gmail.com>
2024-10-03 19:29:54 +0200

    Implemented generic replace and update methods

--- server/src/Controllers/Controller.ts
+++ server/src/Controllers/Controller.ts
@@ -1,6 +1,36 @@
+import mongoose, { type Model } from "mongoose";
 import type { NextFunction, Request, Response } from "express";
 import type { HttpMethod } from "../Entities/HttpMethod.ts";
 
-export class Controller {
+export class Controller<modelT extends Model<any>> {
     constructor() {}
+
+    static async update<T extends Model<any>>(
+        model: T,
+        filter: mongoose.RootFilterQuery<T> | undefined,
+        update: mongoose.UpdateQuery<T>
+    ): Promise<T | null | mongoose.Error.ValidationError> {
+        const doc = await model.findOneAndUpdate(filter, update, {
+            new: true,
+            runValidators: true,
+        });
+
+        console.log(doc);
+
+        return doc;
+    }
+    static async replace<T extends Model<any>>(
+        model: T,
+        filter: mongoose.RootFilterQuery<T> | undefined,
+        update: mongoose.UpdateQuery<T>
+    ): Promise<T | null | mongoose.Error.ValidationError> {
+        const doc = await model.findOneAndReplace(filter, update, {
+            new: true,
+            runValidators: true,
+        });
+
+        console.log(doc);
+
+        return doc;
+    }
 }
