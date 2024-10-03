5557ae9790821e3cdc937e74129b53e5024ebe28
Damián Jambor <jambor.damian@gmail.com>
2024-10-03 11:16:38 +0200

    Get all and DELETE endpoint added

--- server/src/Controllers/TaskController.ts
+++ server/src/Controllers/TaskController.ts
@@ -2,33 +2,43 @@ import type { NextFunction, Request, Response } from "express";
 import type { Routable, RoutingMap } from "../Utils/Router.ts";
 import { Controller } from "./Controller.ts";
 import { HttpStatusCodes } from "../Utils/HttpStatusCodes.ts";
+import { Task } from "../Schemas/Task.ts";
 
 export class TaskController extends Controller implements Routable {
     routes() {
         return {
-            "/task/:id/:x": {
-                GET: this.getAll,
-            },
             "/task/:id": {
-                GET: this.getById,
-                PUT: this.putById,
+                GET: this.getByTaskId,
+                DELETE: this.deleteByTaskId,
+            },
+            "/tasks": {
+                GET: this.getAllTasks,
+                POST: this.postTask,
             },
         };
     }
 
-    getById(req: Request, res: Response, next: NextFunction) {
+    getByTaskId(req: Request, res: Response, next: NextFunction) {
         console.log("GET!");
+        // TODO
 
         res.status(HttpStatusCodes.NOT_IMPLEMENTED).send("GET");
     }
-    putById(req: Request, res: Response, next: NextFunction) {
-        console.log("PUT!");
+    async deleteByTaskId(req: Request, res: Response, next: NextFunction) {
+        const task = await Task.findOne({ _id: req.params.id }).exec();
+        if (!task) return res.status(HttpStatusCodes.NOT_FOUND).send();
 
-        res.status(HttpStatusCodes.NOT_IMPLEMENTED).send("PUT");
+        const result = await Task.deleteOne({ _id: req.params.id }).exec();
+        res.status(HttpStatusCodes.NO_CONTENT).send();
     }
-    getAll(req: Request, res: Response, next: NextFunction) {
-        console.log("GET ALL!");
+    postTask(req: Request, res: Response, next: NextFunction) {
+        // TODO
 
-        res.status(HttpStatusCodes.NOT_IMPLEMENTED).send("GET ALL");
+        res.status(HttpStatusCodes.NOT_IMPLEMENTED).send("POST");
+    }
+    getAllTasks(req: Request, res: Response, next: NextFunction) {
+        Task.find().then((tasks) => {
+            res.status(HttpStatusCodes.OK).send(tasks);
+        });
     }
 }
