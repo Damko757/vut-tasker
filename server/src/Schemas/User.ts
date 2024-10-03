27f2902e38eb68b5a93a9a0f458661b458e387f6
Damián Jambor <jambor.damian@gmail.com>
2024-10-03 21:45:42 +0200

    Better Schame specification Added uniqueValidator plugin

--- server/src/Schemas/User.ts
+++ server/src/Schemas/User.ts
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
 import { type User } from "../../../shared/Entities/User.ts";
 const UserSchema = new mongoose.Schema({
-    nick: String,
-    fullname: String,
-    color: String, //Hex
+    nick: { type: String, unique: true, required: true },
+    fullname: { type: String, required: true },
+    color: { type: String, required: true }, //Hex
     show_my_progress: { type: Boolean, default: true },
     show_me_progress: { type: Boolean, default: true },
 });
 
-const UserModel = mongoose.model<User>("User", UserSchema);
+UserSchema.plugin(uniqueValidator);
+
+export const UserModel = mongoose.model<User>("User", UserSchema);
