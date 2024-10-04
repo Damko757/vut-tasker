40aff8d04348c01edbd941aac025a8c31c8a0f15
Damián Jambor <jambor.damian@gmail.com>
2024-10-04 16:27:24 +0200

    Added capitalize and ISOFDT String prototype methods

---
+++ web/src/extensions/String.extensions.ts
@@ -0,0 +1,22 @@
+declare global {
+    interface String {
+        capitalize(): string;
+
+        ISOToFormattedDateTime(showSeconds?: boolean): string;
+    }
+}
+
+String.prototype.capitalize = function (this: string) {
+    return this[0].toLocaleUpperCase() + this.slice(1).toLocaleLowerCase();
+};
+String.prototype.ISOToFormattedDateTime = function (
+    this: string,
+    showSeconds: boolean = false
+) {
+    const d = new Date(this);
+    return `${d.getDate()}. ${
+        d.getMonth() + 1
+    }. ${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}` + showSeconds
+        ? `:${d.getSeconds()}`
+        : ``;
+};
