2015b54a48614fff5bb3cfe6802cfec51dab0df0
Damián Jambor <jambor.damian@gmail.com>
2024-10-04 16:29:16 +0200

    Implemented showing subject task types

--- web/src/screens/SubjectScreen.vue
+++ web/src/screens/SubjectScreen.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import axios, { HttpStatusCode } from "axios";
-import { ref } from "vue";
-import type { Task } from "../../../shared/Entities/Task";
+import { computed, ref } from "vue";
+import { TaskType, type Task } from "../../../shared/Entities/Task";
+import { API_URL } from "../const";
+import "../Utils.ts";
+import Tasks from "../components/Subject/Tasks.vue";
 
 defineExpose({
     load,
@@ -17,20 +20,33 @@ const props = defineProps({
     },
 });
 
-const subject = ref<Task[] | null>();
+const subjectTasks = ref<Task[] | null>();
+const allTaskTypes = Object.keys(TaskType)
+    .filter((v) => isNaN(Number(v)))
+    .map((v) => v.toLocaleLowerCase())
+    .sort();
+const typeToTasks = computed(() => {
+    const map = new Map<TaskType, Task[]>();
+    allTaskTypes.forEach((type) => map.set(type as unknown as TaskType, []));
+    subjectTasks.value?.forEach((task) => {
+        map.get(task.type)?.push(task);
+    });
+
+    return map;
+});
 
 function load() {
     emit("loadStateChange", 0);
     axios
-        .get<Task[]>(`http://localhost:3000/tasks/${props.subjectName}`)
+        .get<Task[]>(API_URL + `/tasks/${props.subjectName}`)
         .then(async (response) => {
             emit("loadStateChange", 1);
 
-            subject.value = response.data;
+            subjectTasks.value = response.data.length ? response.data : null;
         })
         .catch((response) => {
             if (response.status == HttpStatusCode.NotFound) {
-                subject.value = null;
+                subjectTasks.value = null;
                 emit("loadStateChange", 1);
             } else {
                 emit("loadStateChange", -1);
@@ -39,13 +55,50 @@ function load() {
 }
 </script>
 <template>
-    <div v-if="subject === null" class="fs-2 fw-bold text-center text-danger">
+    <div
+        v-if="subjectTasks === null"
+        class="fs-2 fw-bold text-center text-danger"
+    >
         <em
             ><u>{{ subjectName }}</u></em
         >
         neexistuje :/
     </div>
     <div v-else>
-        {{ subject }}
+        <h1 class="fw-bold px-2">
+            {{ subjectName }}
+        </h1>
+        <div class="types px-4">
+            <section
+                v-for="taskType in allTaskTypes"
+                class="type mb-2"
+                :key="taskType"
+            >
+                <h3 class="fw-bold w-fit-content">
+                    <div class="ps-1 pe-5">
+                        {{ (taskType as unknown as string).capitalize() }}
+                    </div>
+                    <div class="hr"></div>
+                </h3>
+                <div>
+                    <Tasks
+                        :tasks="typeToTasks.get(taskType as unknown as TaskType)"
+                    />
+                </div>
+            </section>
+        </div>
     </div>
 </template>
+<style lang="scss" scoped>
+@import "/src/SCSS/main.scss";
+
+section.type {
+    .hr {
+        margin-top: 0.5em;
+        background-color: $white;
+        min-height: 0.15em;
+        border-radius: 0.15em;
+        width: 100%;
+    }
+}
+</style>
