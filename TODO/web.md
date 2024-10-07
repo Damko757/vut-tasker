447fd8e55d60baf5c85027b93992252f31eadfe4
Damián Jambor <jambor.damian@gmail.com>
2024-10-07 13:31:37 +0200

    SEMI-IMPLEMENTED! HomeScreenShowing upcoming tasks

--- TODO/web.md
+++ TODO/web.md
@@ -2,5 +2,6 @@
 2. Implement saving checking - unchecking ✔
 3. Implement showing who completed what ✔
 4. Implement HomeScreen:
-    - Upcoming tasks
+    - Upcoming tasks ✔
+    - Tasks ViewOnly
 5. Implement switches to enable/disable user prefs for showing stats next to checkbox
--- web/src/screens/HomeScreen.vue
+++ web/src/screens/HomeScreen.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import axios, { HttpStatusCode } from "axios";
 import type { Task } from "../../../shared/Entities/Task";
 import { API_URL } from "../const";
+import TaskView from "../components/Subject/TaskView.vue";
+import Tasks from "../components/Subject/Tasks.vue";
 
 defineExpose({
     load,
@@ -11,7 +13,37 @@ const emit = defineEmits<{
     (e: "loadStateChange", newState: number): void;
 }>();
 
-const tasks = ref<Task[]>([]);
+const allTasks = ref<Task[]>([]);
+
+/**
+ * Returns date, which is used to sort the strings
+ * RULES:
+ * 1. if registration_start => registration_start
+ * 2. if registration_start && registration_end is after Today, then 3.
+ * 3. Due Data
+ * 4. ""
+ */
+function getUpcomingDate(t: Task): string {
+    if (t.registration_date_start) return t.registration_date_start;
+
+    const last = t.registration_date_end || t.registration_date_start;
+    if (last && new Date(last) > new Date()) return last;
+
+    return t.due_date ?? "";
+}
+const sortedTasks = computed(
+    () =>
+        allTasks.value
+            ?.filter((x) => getUpcomingDate(x))
+            .sort((a, b) => {
+                const aVal = getUpcomingDate(a);
+                const bVal = getUpcomingDate(b);
+
+                if (aVal < bVal) return -1;
+                if (aVal > bVal) return 1;
+                return 0;
+            }) ?? []
+);
 
 function load() {
     emit("loadStateChange", 0);
@@ -20,7 +52,7 @@ function load() {
         .then(async (response) => {
             if (response.status == HttpStatusCode.Ok) {
                 emit("loadStateChange", 1);
-                tasks.value = response.data;
+                allTasks.value = response.data;
             } else {
                 emit("loadStateChange", -1);
             }
@@ -30,5 +62,20 @@ function load() {
         });
 }
 </script>
-<template>Hello!</template>
+<template>
+    <h1 class="fw-bold px-2">Najbližšie udalosti:</h1>
+    <div class="types px-4">
+        <section class="type mb-2">
+            <!-- <h3 class="fw-bold w-fit-content">
+                <div class="ps-1 pe-5">
+                    {{ (taskType as unknown as string).capitalize() }}
+                </div>
+                <div class="hr"></div>
+            </h3> -->
+            <div>
+                <Tasks :tasks="sortedTasks" subject="" type="" />
+            </div>
+        </section>
+    </div>
+</template>
 <style lang="scss" scoped></style>
