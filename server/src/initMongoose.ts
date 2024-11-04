2c2e3c713ad6ee35a50658fc2822b2f67c4889f1
Damko757 <jambor.damian@gmail.com>
2024-11-04 11:40:34 +0100

    Fixed mongoDB connection error Normalised container naming

--- server/src/initMongoose.ts
+++ server/src/initMongoose.ts
@@ -5,8 +5,9 @@ import { UserModel } from "./Schemas/User.ts";
 import { USERS } from "../../shared/config/users.ts";
 export const initMongoose = async () => {
     return new Promise<void>((resolve, rejected) => {
-        //const connectString = `mongodb://${ENV.DATABASE_USER}:${ENV.DATABASE_PASSWORD}@${ENV.DATABASE_URI}:${ENV.DATABASE_PORT}`;
-        const connectString = `mongodb://root:root@mongodb:27017/test?authSource=admin`;
+        const connectString = `mongodb://${ENV.DATABASE_USER}:${ENV.DATABASE_PASSWORD}@${ENV.DATABASE_URI}:${ENV.DATABASE_PORT}`;
+        // const connectString = `mongodb://root:root@mongodb:27017/test?authSource=admin`;
+
         mongoose
             .set("strictQuery", false)
             .connect(connectString)
