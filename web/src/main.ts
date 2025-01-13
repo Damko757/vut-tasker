dcbcfee539ba48d00d752e357f4727e3e78b60b7
Damko757 <jambor.damian@gmail.com>
2025-01-13 17:57:43 +0100

    withCredentials is defaultly on

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { Store } from "./store/store.ts";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 createApp(App).provide("store", Store).mount("#app");
