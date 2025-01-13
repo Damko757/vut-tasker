f893f4c6528673e35d5f98c3d58fc6bc8c35ee4b
Damko757 <jambor.damian@gmail.com>
2025-01-13 16:27:18 +0100

    Fixed tsc errors

--- shared/config/globals.ts
+++ shared/config/globals.ts
@@ -1,30 +1,33 @@
 import type { User } from "../Entities/User.ts";
 
 interface Globals {
-    USERS: User[];
+  USERS: User[];
 }
 export const Globals: Globals = {
-    USERS: [
-        {
-            nick: "User1",
-            fullname: "Userus",
-            color: "#008000",
-            show_my_progress: true,
-            show_me_progress: true,
-        },
-        {
-            nick: "User2",
-            fullname: "Serus",
-            color: "#250018",
-            show_my_progress: true,
-            show_me_progress: true,
-        },
-        {
-            nick: "User3",
-            fullname: "Sures",
-            color: "#123456",
-            show_my_progress: true,
-            show_me_progress: true,
-        },
-    ],
+  USERS: [
+    // {
+    //   nick: "User1",
+    //   fullname: "Userus",
+    //   color: "#008000",
+    //   subscribed_subjects: [],
+    //   show_my_progress: true,
+    //   show_me_progress: true,
+    // },
+    // {
+    //   nick: "User2",
+    //   fullname: "Serus",
+    //   subscribed_subjects: [],
+    //   color: "#250018",
+    //   show_my_progress: true,
+    //   show_me_progress: true,
+    // },
+    // {
+    //   nick: "User3",
+    //   fullname: "Sures",
+    //   subscribed_subjects: [],
+    //   color: "#123456",
+    //   show_my_progress: true,
+    //   show_me_progress: true,
+    // },
+  ],
 };
--- shared/config/users.ts
+++ shared/config/users.ts
@@ -1,25 +1,28 @@
 import type { User } from "../Entities/User.ts";
 
 export const USERS: User[] = [
-    {
-        nick: "User1",
-        fullname: "Userus",
-        color: "#008000",
-        show_my_progress: true,
-        show_me_progress: true,
-    },
-    {
-        nick: "User2",
-        fullname: "Serus",
-        color: "#250018",
-        show_my_progress: true,
-        show_me_progress: true,
-    },
-    {
-        nick: "User3",
-        fullname: "Sures",
-        color: "#123456",
-        show_my_progress: true,
-        show_me_progress: true,
-    },
+  {
+    nick: "User1",
+    fullname: "Userus",
+    subscribed_subjects: [],
+    color: "#008000",
+    show_my_progress: true,
+    show_me_progress: true,
+  },
+  {
+    nick: "User2",
+    fullname: "Serus",
+    subscribed_subjects: [],
+    color: "#250018",
+    show_my_progress: true,
+    show_me_progress: true,
+  },
+  {
+    nick: "User3",
+    fullname: "Sures",
+    subscribed_subjects: [],
+    color: "#123456",
+    show_my_progress: true,
+    show_me_progress: true,
+  },
 ];
