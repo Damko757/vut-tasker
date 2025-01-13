4679850f2798c269ec0ba287981e83787158bd13
Damko757 <jambor.damian@gmail.com>
2025-01-13 17:58:32 +0100

    Formatting

--- web/src/screens/UserSelectScreen.vue
+++ web/src/screens/UserSelectScreen.vue
@@ -9,38 +9,38 @@ import { ref } from "vue";
 const cookies = useCookies([CookieValue.USER]);
 
 function onUserClick(user: User) {
-    cookies.set(CookieValue.USER, user.nick, {
-        sameSite: "strict",
-        expires: (function (d = new Date()) {
-            d.setDate(d.getDate() + 365);
-            return d;
-        })(),
-    });
+  cookies.set(CookieValue.USER, user.nick, {
+    sameSite: "strict",
+    expires: (function (d = new Date()) {
+      d.setDate(d.getDate() + 365);
+      return d;
+    })(),
+  });
 }
 
 const users = ref<User[]>();
 
 axios.get<User[]>(API_URL + "/users").then((response) => {
-    users.value = response.data;
+  users.value = response.data;
 });
 </script>
 <template>
-    <h1 class="fw-bold p-2">Hello! Who are you?</h1>
-    <div class="d-flex justify-content-center align-items-center flex-wrap">
-        <UserComponent
-            v-if="users != undefined"
-            v-for="user in users"
-            :key="user.nick"
-            :user="user"
-            @click="onUserClick(user)"
-        />
-        <div v-else class="fst-italic">Loading...</div>
-    </div>
+  <h1 class="fw-bold p-2">Hello! Who are you?</h1>
+  <div class="d-flex justify-content-center align-items-center flex-wrap">
+    <UserComponent
+      v-if="users != undefined"
+      v-for="user in users"
+      :key="user.nick"
+      :user="user"
+      @click="onUserClick(user)"
+    />
+    <div v-else class="fst-italic">Loading...</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @import "/src/SCSS/main.scss";
 h1 {
-    color: $white;
+  color: $white;
 }
 </style>
