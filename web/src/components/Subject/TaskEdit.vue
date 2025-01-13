e428fa3ebf47387dc377a6897653a4d78a1f5197
Damko757 <jambor.damian@gmail.com>
2025-01-13 17:13:04 +0100

    Implemented simplier task system

--- web/src/components/Subject/TaskEdit.vue
+++ web/src/components/Subject/TaskEdit.vue
@@ -7,113 +7,110 @@ import axios from "axios";
 import { API_URL } from "../../const";
 
 const props = defineProps({
-    task: {
-        type: Object as PropType<Partial<Task> | null>,
-        required: false,
-        default: null,
-    },
-    addOrEdit: {
-        type: String,
-        default: "edit",
-    },
+  task: {
+    type: Object as PropType<Partial<Task> | null>,
+    required: false,
+    default: null,
+  },
+  addOrEdit: {
+    type: String,
+    default: "edit",
+  },
 });
 
 const edittedTask = ref<Partial<Task>>({});
 watchEffect(() => {
-    edittedTask.value = props.task ?? {};
+  edittedTask.value = props.task ?? {};
 });
 
 const submit = () => {
-    const promise = props.task?._id
-        ? axios.patch<Task>(
-              API_URL + `/task/${props.task._id}`,
-              edittedTask.value
-          )
-        : axios.post<Task>(API_URL + `/tasks`, edittedTask.value);
+  const promise = props.task?._id
+    ? axios.patch<Task>(API_URL + `/task/${props.task._id}`, edittedTask.value)
+    : axios.post<Task>(API_URL + `/tasks`, edittedTask.value);
 
-    promise
-        .then((response) => {
-            emit("done", response.data as Task);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  promise
+    .then((response) => {
+      emit("done", response.data as Task);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 function stateChanged(ns: boolean) {
-    edittedTask.value.required = ns;
+  edittedTask.value.required = ns;
 }
 
 const emit = defineEmits<{
-    (e: "done", task: Task | null): void;
+  (e: "done", task: Task | null): void;
 }>();
 </script>
 <template>
-    <h5 class="fw-bold position-relative">
-        {{ addOrEdit == "edit" ? "Edit" : "Add" }} task
-    </h5>
-    <div v-if="edittedTask.due_date" class="due-date fst-italic">
-        &#40;{{ edittedTask.due_date?.ISOToFormattedDateTime() }}&#41;
+  <h5 class="fw-bold position-relative">
+    {{ addOrEdit == "edit" ? "Edit" : "Add" }} task
+  </h5>
+  <div v-if="edittedTask.due_date" class="due-date fst-italic">
+    &#40;{{ edittedTask.due_date?.ISOToFormattedDateTime()
+    }}{{
+      edittedTask.due_date_end
+        ? ` &hyphen; ${edittedTask.due_date_end.ISOToFormattedDateTime()}`
+        : ``
+    }}&#41;
+  </div>
+  <div class="editables">
+    <div>
+      <span class="fw-bold">Description: </span
+      ><span><SimpleInput v-model="edittedTask.description" /></span>
+      <br />
+      <span class="fw-bold">Due date: </span
+      ><span
+        ><SimpleInput v-model="edittedTask.due_date" type="datetime-local" /> -
+      </span>
+      <span
+        ><SimpleInput v-model="edittedTask.due_date_end" type="datetime-local"
+      /></span>
+      <br />
+      <span class="fw-bold">Required: </span
+      ><span class="d-inline-block ps-2 my-2"
+        ><CheckBox
+          :state="edittedTask.required"
+          :grey-out="false"
+          @state-change="stateChanged"
+      /></span>
+      <span class="d-inline-block" style="width: 2em"></span>
+      <span class="fw-bold">Personal only: </span
+      ><span class="d-inline-block ps-2 my-2"
+        ><CheckBox
+          :state="edittedTask.personal"
+          :grey-out="false"
+          @state-change="stateChanged"
+      /></span>
+      <br />
+      <span class="fw-bold">Link: </span
+      ><a
+        ><SimpleInput
+          v-model="edittedTask.link"
+          type="text"
+          style="color: cyan"
+      /></a>
     </div>
-    <div class="editables">
-        <div>
-            <span class="fw-bold">Description: </span
-            ><span><SimpleInput v-model="edittedTask.description" /></span>
-            <br />
-            <span class="fw-bold">Due date: </span
-            ><span
-                ><SimpleInput
-                    v-model="edittedTask.due_date"
-                    type="datetime-local"
-            /></span>
-            <br />
-            <span class="fw-bold">Required: </span
-            ><span class="d-inline-block ps-2 my-2"
-                ><CheckBox
-                    :state="edittedTask.required"
-                    :grey-out="false"
-                    @state-change="stateChanged"
-            /></span>
-            <br />
-            <span class="fw-bold">Registration: </span
-            ><span class="d-block"
-                ><SimpleInput
-                    v-model="edittedTask.registration_date_start"
-                    type="datetime-local" />
-                <span class="fw-bold"> - </span>
-                <SimpleInput
-                    v-model="edittedTask.registration_date_end"
-                    type="datetime-local"
-            /></span>
-            <br />
-            <span class="fw-bold">Link: </span
-            ><a
-                ><SimpleInput
-                    v-model="edittedTask.link"
-                    type="text"
-                    style="color: cyan"
-            /></a>
-        </div>
 
-        <div class="pt-2 fw-bold">
-            <button class="btn btn-success mx-3 fw-bold" @click="submit()">
-                Submit!
-            </button>
-            <button
-                class="btn btn-danger mx-3 fw-bold"
-                @click="emit('done', null)"
-            >
-                Cancel!
-            </button>
-        </div>
+    <div class="pt-2 fw-bold">
+      <button class="btn btn-success mx-3 fw-bold" @click="submit()">
+        Submit!
+      </button>
+      <button class="btn btn-danger mx-3 fw-bold" @click="emit('done', null)">
+        Cancel!
+      </button>
     </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 @import "/src/SCSS/main.scss";
-.due-date{
-    color: darken($white, 10%);
+.due-date {
+  color: darken($white, 10%);
 }
 .editables {
-    text-decoration: none !important;
+  text-decoration: none !important;
 }
 </style>
--- web/src/components/Subject/TaskView.vue
+++ web/src/components/Subject/TaskView.vue
@@ -3,98 +3,81 @@ import type { PropType } from "vue";
 import type { Task } from "../../../../shared/Entities/Task";
 
 const props = defineProps({
-    task: {
-        type: Object as PropType<Task>,
-        required: true,
-    },
-    isCollapsed: { type: Boolean, required: true },
-    showAll: { type: Boolean, default: false },
+  task: {
+    type: Object as PropType<Task>,
+    required: true,
+  },
+  isCollapsed: { type: Boolean, required: true },
+  showAll: { type: Boolean, default: false },
 });
 
 const emit = defineEmits<{
-    (e: "update", task: Task): void;
-    (e: "delete", task: Task): void;
+  (e: "update", task: Task): void;
+  (e: "delete", task: Task): void;
 }>();
 </script>
 <template>
-    <div class="cursor-pointer">
-        <h5 class="fw-bold position-relative pe-3">
-            {{ task.required ? "*" : "" }}{{ task.description }}
-            <template v-if="showAll">
-                <span class="fw-bold"
-                    >(<u>{{ task.subject }}</u> -
-                    {{ (task.type as unknown as string).capitalize() }})</span
-                >
-            </template>
-            <div
-                class="collapse-arrow"
-                :class="{ collapsed: props.isCollapsed }"
-            >
-                >
-            </div>
-        </h5>
-        <div v-if="task.due_date" class="due-date fst-italic">
-            &#40;{{ task.due_date?.ISOToFormattedDateTime() }}&#41;
-        </div>
-        <div
-            class="h-0 overflow-hidden"
-            :class="{ collapsed: props.isCollapsed }"
+  <div class="cursor-pointer">
+    <h5 class="fw-bold position-relative pe-3">
+      {{ task.required ? "*" : "" }}{{ task.description }}
+      <template v-if="showAll">
+        <span class="fw-bold"
+          >(<u>{{ task.subject }}</u> -
+          {{ (task.type as unknown as string).capitalize() }})</span
         >
-            <div>
-                <span class="fw-bold">Registration: </span
-                ><span class="">{{
-                    task.registration_date_start
-                        ? `${task.registration_date_start.ISOToFormattedDateTime()}` +
-                          (task.registration_date_end
-                              ? `- ${task.registration_date_end.ISOToFormattedDateTime()}`
-                              : ``)
-                        : "&lt;No term&gt;"
-                }}</span>
-                <br />
-                <span class="fw-bold">Link: </span
-                ><a :href="task.link ?? '#'">{{ task.link ?? "-" }}</a>
-            </div>
-
-            <div class="pt-2 fw-bold">
-                <button
-                    class="btn btn-success"
-                    @click.stop="emit('update', task)"
-                >
-                    Update
-                </button>
-                <button
-                    class="btn btn-danger mx-3"
-                    @click.stop="emit('delete', task)"
-                >
-                    Delete
-                </button>
-            </div>
-        </div>
+      </template>
+      <div class="collapse-arrow" :class="{ collapsed: props.isCollapsed }">
+        >
+      </div>
+    </h5>
+    <div v-if="task.due_date" class="due-date fst-italic">
+      &#40;{{ task.due_date?.ISOToFormattedDateTime()
+      }}{{
+        task.due_date_end
+          ? ` &hyphen; ${task.due_date_end.ISOToFormattedDateTime()}`
+          : ``
+      }}&#41;
     </div>
+    <div class="h-0 overflow-hidden" :class="{ collapsed: props.isCollapsed }">
+      <div>
+        <span class="fw-bold">Link: </span
+        ><a :href="task.link ?? '#'">{{ task.link ?? "&hyphen;" }}</a>
+      </div>
+
+      <div class="pt-2 fw-bold" v-if="!showAll">
+        <button class="btn btn-success" @click.stop="emit('update', task)">
+          Update
+        </button>
+        <button class="btn btn-danger mx-3" @click.stop="emit('delete', task)">
+          Delete
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 @import "/src/SCSS/main.scss";
 
 .due-date {
-    color: darken($white, 10%);
+  color: darken($white, 10%);
 }
 
 .collapsed {
-    height: auto !important;
+  height: auto !important;
 }
 
 .collapse-arrow {
-    position: absolute;
-    font-size: 1.75em;
-    right: 0;
-    top: 50%;
-    translate: 0% -50%;
-    rotate: 90deg;
-    transition: 250ms;
-    transform-origin: center;
+  position: absolute;
+  font-size: 1.75em;
+  right: 0;
+  top: 50%;
+  translate: 0% -50%;
+  rotate: 90deg;
+  transition: 250ms;
+  transform-origin: center;
 
-    &.collapsed {
-        rotate: -90deg !important;
-    }
+  &.collapsed {
+    rotate: -90deg !important;
+  }
 }
 </style>
