5e28c2fd96c0ac8d773c5906363a9c64fea80923
Damko757 <jambor.damian@gmail.com>
2025-01-13 17:55:49 +0100

    Specified CORS origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import express, {
-    type NextFunction,
-    type Request,
-    type Response,
+  type NextFunction,
+  type Request,
+  type Response,
 } from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -12,27 +12,33 @@ import { HttpStatusCodes } from "./Utils/HttpStatusCodes.ts";
 import { Router } from "./Utils/Router.ts";
 import { routableControllers } from "./Utils/RoutableControllers.ts";
 import { errorHandler } from "./Utils/ErrorHandler.ts";
+import { ENV } from "./const.ts";
 
 export const initApp = () => {
-    const app = express();
+  const app = express();
 
-    app.use(helmet());
-    app.use(cors());
-    app.use(bodyParser.json());
-    app.use(cookieParser());
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: ENV.WEB_URL,
+      credentials: true,
+    })
+  );
+  app.use(bodyParser.json());
+  app.use(cookieParser());
 
-    const router = new Router(routableControllers);
-    router.createRoutes(app);
+  const router = new Router(routableControllers);
+  router.createRoutes(app);
 
-    app.use((req: Request, res: Response, next: NextFunction) => {
-        const errorMsg: ErrorResponse = {
-            message: `Unknown request: ${req.method} ${req.url}`,
-        };
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const errorMsg: ErrorResponse = {
+      message: `Unknown request: ${req.method} ${req.url}`,
+    };
 
-        res.status(HttpStatusCodes.BAD_REQUEST).send(errorMsg);
-    });
+    res.status(HttpStatusCodes.BAD_REQUEST).send(errorMsg);
+  });
 
-    app.use(errorHandler);
+  app.use(errorHandler);
 
-    return app;
+  return app;
 };
