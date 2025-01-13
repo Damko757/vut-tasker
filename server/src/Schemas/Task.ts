56698a184bca8a274b67012a29030bef03c03d37
Damko757 <jambor.damian@gmail.com>
2025-01-13 16:30:56 +0100

    Task have now different structure

--- server/src/Schemas/Task.ts
+++ server/src/Schemas/Task.ts
@@ -4,36 +4,38 @@ import type { User } from "../../../shared/Entities/User.ts";
 
 const taskTypes = [];
 for (const taskType in TaskType) {
-    if (isNaN(Number(taskType))) taskTypes.push(taskType.toLowerCase());
+  if (isNaN(Number(taskType))) taskTypes.push(taskType.toLowerCase());
 }
 
 const TaskSchema = new mongoose.Schema({
-    subject: { type: String, required: true },
-    type: {
-        type: String,
-        required: true,
-        enum: {
-            values: taskTypes,
-            message: `Type '{VALUE}' is not in allowed types: ${taskTypes.join(
-                ", "
-            )}.`,
-        },
+  subject: { type: String, required: true },
+  type: {
+    type: String,
+    required: true,
+    enum: {
+      values: taskTypes,
+      message: `Type '{VALUE}' is not in allowed types: ${taskTypes.join(
+        ", "
+      )}.`,
     },
-    required: {
-        type: Boolean,
-        default: true,
-    },
-    description: { type: String, required: false, default: null },
-    due_date: { type: String, required: false, default: null },
-    registration_date_start: { type: String, required: false, default: null },
-    registration_date_end: { type: String, required: false, default: null },
-    link: { type: String, required: false, default: null },
-    completed_by: { type: [String], required: true },
+  },
+  required: {
+    type: Boolean,
+    default: true,
+  },
+  description: { type: String, required: false, default: null },
+  due_date: { type: String, required: false, default: null },
+  due_date_end: { type: String, required: false, default: null },
+  personal: { type: Boolean, default: false },
+  // registration_date_start: { type: String, required: false, default: null },
+  // registration_date_end: { type: String, required: false, default: null },
+  link: { type: String, required: false, default: null },
+  completed_by: { type: [String], required: true },
 });
 
 TaskSchema.pre("validate", function (next, ...args) {
-    if (this.get("type")) this.set("type", this.get("type").toLowerCase());
-    next();
+  if (this.get("type")) this.set("type", this.get("type").toLowerCase());
+  next();
 });
 
 export const TaskModel = mongoose.model<_Task>("Task", TaskSchema);
