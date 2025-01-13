7429e8776f08d20952e6e2d01da08263bedcef6e
Damko757 <jambor.damian@gmail.com>
2025-01-13 16:30:56 +0100

    Task have now different structure

--- shared/Entities/Task.ts
+++ shared/Entities/Task.ts
@@ -1,19 +1,20 @@
 import type { date } from "../Utils.ts";
 
 export interface Task {
-    _id?: string;
-    subject: string;
-    type: TaskType;
-    required: boolean;
-    description: string;
-    due_date: date | null;
-    registration_date_start: date | null;
-    registration_date_end: date | null;
-    link: string | null;
-    completed_by: string[]; // nicks
+  _id?: string;
+  subject: string;
+  type: TaskType;
+  required: boolean;
+  description: string;
+  due_date: date | null;
+  due_date_end: date | null;
+  personal: boolean;
+  link: string | null;
+  completed_by: string[]; // nicks
 }
 export enum TaskType {
-    PROJECT,
-    HOMEWORK,
-    EXAM,
+  PROJECT,
+  HOMEWORK,
+  EXAM,
+  REGISTRATION,
 }
