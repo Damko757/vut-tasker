34d38a0196ad01113564baf738036d5e3a06e4d2
Damko757 <jambor.damian@gmail.com>
2025-01-14 19:20:50 +0100

    No nick user will return itself

--- server/src/Controllers/UserController.ts
+++ server/src/Controllers/UserController.ts
@@ -5,83 +5,80 @@ import { Controller } from "./Controller.ts";
 import { HttpStatusCodes } from "../Utils/HttpStatusCodes.ts";
 import type { MiddlewareFunction } from "../Entities/MiddlewareFunction.ts";
 import { TaskModel } from "../Schemas/Task.ts";
+import { CookieValue } from "../Utils/Utils.ts";
 
 export class UserController
-    extends Controller<typeof UserModel>
-    implements Routable
+  extends Controller<typeof UserModel>
+  implements Routable
 {
-    routes() {
-        return {
-            "/users": {
-                GET: this.getAllUsers,
-                POST: this.postUser,
-            },
-            "/user/:nick": {
-                GET: this.getUserByNick,
-                PUT: this.putUserByNick,
-                PATCH: this.patchUserByNick,
-                DELETE: this.deleteUserByNick,
-            },
-        };
-    }
+  routes() {
+    return {
+      "/users": {
+        GET: this.getAllUsers,
+        POST: this.postUser,
+      },
+      "/user/:nick": {
+        GET: this.getUserByNick,
+        PUT: this.putUserByNick,
+        PATCH: this.patchUserByNick,
+        DELETE: this.deleteUserByNick,
+      },
+    };
+  }
 
-    async getAllUsers(req: Request, res: Response, next: NextFunction) {
-        res.status(HttpStatusCodes.OK).send(await UserModel.find().exec());
-    }
+  async getAllUsers(req: Request, res: Response, next: NextFunction) {
+    res.status(HttpStatusCodes.OK).send(await UserModel.find().exec());
+  }
 
-    async postUser(req: Request, res: Response, next: NextFunction) {
-        const user = new UserModel(req.body);
+  async postUser(req: Request, res: Response, next: NextFunction) {
+    const user = new UserModel(req.body);
 
-        return user
-            .save()
-            .catch((error) => {
-                return res
-                    .status(HttpStatusCodes.UNPROCESSABLE_ENTITY)
-                    .send(error);
-            })
-            .then(async () => {
-                return res.status(HttpStatusCodes.OK).send(user);
-            });
-    }
+    return user
+      .save()
+      .catch((error) => {
+        return res.status(HttpStatusCodes.UNPROCESSABLE_ENTITY).send(error);
+      })
+      .then(async () => {
+        return res.status(HttpStatusCodes.OK).send(user);
+      });
+  }
 
-    async getUserByNick(req: Request, res: Response, next: NextFunction) {
-        const user = await UserModel.findOne({ nick: req.params.nick }).exec();
+  async getUserByNick(req: Request, res: Response, next: NextFunction) {
+    const user = await UserModel.findOne({
+      nick: req.params.nick ?? req.cookies[CookieValue.USER] ?? "",
+    }).exec();
 
-        if (user) return res.send(user);
-        return res.status(HttpStatusCodes.NOT_FOUND).send();
-    }
-    async putUserByNick(req: Request, res: Response, next: NextFunction) {
-        Controller.replace(UserModel, { nick: req.params.nick }, req.body)
-            .then((updated) => {
-                return updated
-                    ? res.status(HttpStatusCodes.OK).send(updated)
-                    : res.status(HttpStatusCodes.NOT_FOUND).send();
-            })
-            .catch((error) => {
-                return res
-                    .status(HttpStatusCodes.UNPROCESSABLE_ENTITY)
-                    .send(error);
-            });
-    }
-    async patchUserByNick(req: Request, res: Response, next: NextFunction) {
-        Controller.update(UserModel, { nick: req.params.nick }, req.body)
-            .then((updated) => {
-                return updated
-                    ? res.status(HttpStatusCodes.OK).send(updated)
-                    : res.status(HttpStatusCodes.NOT_FOUND).send();
-            })
-            .catch((error) => {
-                return res
-                    .status(HttpStatusCodes.UNPROCESSABLE_ENTITY)
-                    .send(error);
-            });
-    }
-    async deleteUserByNick(req: Request, res: Response, next: NextFunction) {
-        const result = await Controller.delete(UserModel, {
-            nick: req.params.nick,
-        });
+    if (user) return res.send(user);
+    return res.status(HttpStatusCodes.NOT_FOUND).send();
+  }
+  async putUserByNick(req: Request, res: Response, next: NextFunction) {
+    Controller.replace(UserModel, { nick: req.params.nick }, req.body)
+      .then((updated) => {
+        return updated
+          ? res.status(HttpStatusCodes.OK).send(updated)
+          : res.status(HttpStatusCodes.NOT_FOUND).send();
+      })
+      .catch((error) => {
+        return res.status(HttpStatusCodes.UNPROCESSABLE_ENTITY).send(error);
+      });
+  }
+  async patchUserByNick(req: Request, res: Response, next: NextFunction) {
+    Controller.update(UserModel, { nick: req.params.nick }, req.body)
+      .then((updated) => {
+        return updated
+          ? res.status(HttpStatusCodes.OK).send(updated)
+          : res.status(HttpStatusCodes.NOT_FOUND).send();
+      })
+      .catch((error) => {
+        return res.status(HttpStatusCodes.UNPROCESSABLE_ENTITY).send(error);
+      });
+  }
+  async deleteUserByNick(req: Request, res: Response, next: NextFunction) {
+    const result = await Controller.delete(UserModel, {
+      nick: req.params.nick,
+    });
 
-        if (result) return res.status(HttpStatusCodes.NO_CONTENT).send();
-        else return res.status(HttpStatusCodes.NOT_FOUND).send();
-    }
+    if (result) return res.status(HttpStatusCodes.NO_CONTENT).send();
+    else return res.status(HttpStatusCodes.NOT_FOUND).send();
+  }
 }
