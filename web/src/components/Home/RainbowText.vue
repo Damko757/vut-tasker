0735bdcd68587e730228bec5b897ae8e1c39c149
Damko757 <jambor.damian@gmail.com>
2025-01-14 22:04:11 +0100

    Added simple element for rainbow text

---
+++ web/src/components/Home/RainbowText.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    required: true,
+    type: String,
+  },
+  amount: {
+    default: 5,
+    type: Number,
+  },
+});
+</script>
+<template>
+  <span
+    v-for="(letter, i) in text"
+    :style="{ filter: `hue-rotate(${amount * i}deg)` }"
+    >{{ letter }}</span
+  >
+</template>
