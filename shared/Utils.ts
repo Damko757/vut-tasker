56ddc3f7125d05eed9543201cd1110fdc0ffe5db
Damko757 <jambor.damian@gmail.com>
2025-01-30 14:31:04 +0100

    Added permutations and smallerFirstSort methods

--- shared/Utils.ts
+++ shared/Utils.ts
@@ -1,1 +1,18 @@
 export type date = string;
+
+export function permutations<T>(arr: T[]): T[][] {
+  if (arr.length == 0) return [[]];
+  const out: T[][] = [];
+  arr.forEach((el, i) => {
+    const permsWithoutEl = permutations(arr.filter((x, ii) => ii != i));
+    const permsWithEl = permsWithoutEl.map((x) => [el, ...x]);
+    permsWithEl.forEach((perm) => out.push(perm));
+  });
+  return out;
+}
+
+export function smallerFirstSort<T>(a: T, b: T): -1 | 0 | 1 {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+}
--- web/src/Utils.ts
+++ web/src/Utils.ts
@@ -2,20 +2,3 @@ import "./extensions/String.extensions.ts";
 
 export const roundTo = (n: number, precision: number = 2) =>
   Math.round(n * Math.pow(10, precision)) / Math.pow(10, precision);
-
-export function permutations<T>(arr: T[]): T[][] {
-  if (arr.length == 0) return [[]];
-  const out: T[][] = [];
-  arr.forEach((el, i) => {
-    const permsWithoutEl = permutations(arr.filter((x, ii) => ii != i));
-    const permsWithEl = permsWithoutEl.map((x) => [el, ...x]);
-    permsWithEl.forEach((perm) => out.push(perm));
-  });
-  return out;
-}
-
-export function smallerFirstSort<T>(a: T, b: T): -1 | 0 | 1 {
-  if (a < b) return -1;
-  if (a > b) return 1;
-  return 0;
-}
