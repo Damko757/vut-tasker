67f9b88533c78b16a56dcb4ec22598445e7c1ea6
Damko757 <jambor.damian@gmail.com>
2025-01-31 08:51:12 +0100

    Room section

---
+++ web/src/components/Form/RoomInputs/RoomInputs.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Room } from "../../../../../shared/Entities/Room";
+import SimpleTextArea from "../../Subject/SimpleTextArea.vue";
+import RawRoomInputParse from "./RawRoomInputParse.vue";
+
+const rooms = defineModel<Room[]>();
+</script>
+<template>
+  <RawRoomInputParse @parse="" />
+</template>
