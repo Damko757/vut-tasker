824bfd353eb615b115bd3e5dbec58986747c5eaa
Damko757 <jambor.damian@gmail.com>
2025-02-05 21:14:15 +0100

    Handling empty required values

--- web/src/components/Subject/TaskEdit.vue
+++ web/src/components/Subject/TaskEdit.vue
@@ -33,15 +33,20 @@ const originalRoom = ref(
 );
 const modifiableRoom = ref(`${originalRoom.value}`);
 const submit = () => {
+  if (!edittedTask.value.name?.trim() || !edittedTask.value.due_date) {
+    alert("Missing name or due-date");
+    return;
+  }
+
   const taskToSend: Omit<
     Task,
     "created_by" | "rooms" | "completed_by" | "_id"
   > = {
     name: edittedTask.value.name!.trim(),
-    description: edittedTask.value.description!.trim(),
+    description: edittedTask.value.description?.trim() ?? "",
     due_date: edittedTask.value.due_date!,
-    due_date_end: edittedTask.value.due_date_end!,
-    link: edittedTask.value.link!.trim(),
+    due_date_end: edittedTask.value.due_date_end ?? null,
+    link: edittedTask.value.link?.trim() ?? "",
     personal: edittedTask.value.personal!,
     required: edittedTask.value.required!,
     subject: edittedTask.value.subject!,
